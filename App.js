05ee5286c21df049c97f82bafdb2c009eaa52be3
Xi Chen <xichen.u@gmail.com>
2019-12-01 13:20:32 -0500

    implement something like redux

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import IndexScreen from './src/screens/IndexScreen';
-import { BlogProvider } from './src/context/BlogContext';
+import { Provider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator({
   Index: IndexScreen
@@ -18,6 +18,6 @@ const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider><App /></BlogProvider>
+    <Provider><App /></Provider>
   );
 }
