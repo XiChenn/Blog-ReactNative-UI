29cda6a7caf69ef0993fe7a40cfebcb35e931fc9
Xi Chen <xichen.u@gmail.com>
2019-12-01 11:42:12 -0500

    use reducer

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
 
 import BlogContext from '../context/BlogContext';
+//import { FlatList } from 'react-native-gesture-handler';
 
 const IndexScreen = () => {
-    const value = useContext(BlogContext);
+    const { data, addBlogPost } = useContext(BlogContext);
     return (
         <View>
             <Text>Index Screen</Text>
-            <Text>{value}</Text>
+            <Button title="Add post" onPress={addBlogPost}/>
+            <FlatList
+                data={data}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return <Text>{item.title}</Text>
+                }}
+            />
         </View>
     );
 };
